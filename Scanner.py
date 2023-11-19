65c18eaa95659f14f72f42e1615120ef3a5d6cde
George Răbuș <144466896+gelordo@users.noreply.github.com>
2023-11-19 08:56:01 +0200

    Delete Scanner.py

--- Scanner.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import zxing
-image_path = 'path_to_your_image.jpg'
-reader = zxing.BarCodeReader()
-barcode = reader.decode(image_path)
-decoded_text = barcode.parsed if barcode else "No Data Matrix code found."
-print(decoded_text)
